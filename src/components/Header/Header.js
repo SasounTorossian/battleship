89a3d96d1872dfe4a7a39cbcf543baac9266b8c2
Sasoun Torossian <sasountorossian@gmail.com>
2021-05-18 19:06:59 +0100

    Added header component.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <p>BATTLESHIP</p>
+        </div>
+    )
+}
+
+export default Header
