1e76b5f90cb1c1d27cb36d770e1ca5306835adc4
Sasoun Torossian <sasountorossian@gmail.com>
2021-05-18 19:30:33 +0100

    Initial commit of App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import React, {useState, UseEffect} from "react"
+import Header from "./components/Header/Header"
+import Gameboards from "./components/Gameboards/Gameboards"
+import Dock from "./components/Dock/Dock"
+import gameEngineModule from "./logic/gameEngineModule"
 
 function App() {
+ const [players, setplayers] = useState([])
+ const gameEngine = gameEngineModule()
+
+ // FIXME: Getting called multiple times. Only call once. UseEffect
+ gameEngine.initializePlayers()
+ gameEngine.initializePlayersGameboard()
+ gameEngine.initializeAIGameboard()
+ setplayers(gameEngine.getPlayers())
+
   return (
     <div className="App">
+      {/* <Header />
+      <Gameboards />
+      <Dock /> */}
     </div>
   );
 }
