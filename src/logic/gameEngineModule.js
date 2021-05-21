430149c3c3d69963319764fff0c25ef6a77b2374
Sasoun Torossian <sasountorossian@gmail.com>
2021-05-21 15:35:35 +0100

    Added helper functions to gameEngine return object

--- src/logic/gameEngineModule.js
+++ src/logic/gameEngineModule.js
@@ -16,30 +16,24 @@ const gameEngineModule = () => {
     }   
 
     const initializeAIGameboard = () => {
-        players[1].gameboard.placeAllShips(players[1].fleet.fleet)
+        players[1].gameboard.placeAllShips(players[1].fleet.ships)
     }
 
-    const getPlayers = () => {
-        return players
-    }
+    const getPlayers = () => { return players }
 
-    const getCurrentPlayer = () => {
-        return currentPlayer
-    }
+    const getCurrentPlayer = () => { return currentPlayer }
 
-    const nextPlayerTurn = () => {
-        currentPlayer ^= 1 
-    }
+    const nextPlayerTurn = () => { currentPlayer ^= 1 }
 
     const checkGameOver = () => {
-        return players.ever(player => {
-            player.fleet.CheckIfFleetSunk()
+        return players.some(player => {
+            player.fleet.checkIfShipsSunk()
         })
     }
 
     const checkWhoWon = () => {
-        if(players[0].fleet.CheckIfFleetSunk()) return 1
-        else if(players[1].fleet.CheckIfFleetSunk()) return 0
+        if(players[0].fleet.checkIfShipsSunk()) return 0
+        else if(players[1].fleet.checkIfShipsSunk()) return 1
     }
 
     return {
@@ -47,7 +41,11 @@ const gameEngineModule = () => {
         initializePlayers,
         initializePlayersGameboard,
         initializeAIGameboard,
-        getPlayers
+        getPlayers,
+        getCurrentPlayer,
+        nextPlayerTurn,
+        checkGameOver,
+        checkWhoWon
     }
 }
 
