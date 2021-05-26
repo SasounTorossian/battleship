20c5e2e57defe99bf9346781533a53b55a81f1db
Sasoun Torossian <sasountorossian@gmail.com>
2021-05-26 16:51:14 +0100

    Define hit() and isSunk() functions once to make debugging easier. Removed 'length' variable.

--- src/logic/factories/fleetFactory/fleetFactory.js
+++ src/logic/factories/fleetFactory/fleetFactory.js
@@ -1,96 +1,72 @@
 const fleetFactory = () => {
-    const ships = [
-        {
-            type: "Destroyer",
-            length: 2,
-            position: [],
-            orientation: [
-                [0, 1],
-                [0, 10]
-            ],
-            hit(segment) {
-                if(segment < this.position.length) {
-                    this.position[segment] = "X"
-                }
-            },
-            isSunk() {
-                return !!this.position.length && this.position.every(segment => segment === "X")
-            },
-        },
-        {
-            type: "Submarine",
-            length: 3,
-            position: [],
-            orientation: [
-                [0, 1, 2],
-                [0, 10, 20]
-            ],
-            hit(segment) {
-                if(segment < this.position.length) {
-                    this.position[segment] = "X"
-                }
-            },
-            isSunk() {
-                return !!this.position.length && this.position.every(segment => segment === "X")
-            },
-        },
-        {
-            type: "Cruiser",
-            length: 3,
-            position: [],
-            orientation: [
-                [0, 1, 2],
-                [0, 10, 20]
-            ],
-            hit(segment) {
-                if(segment < this.position.length) {
-                    this.position[segment] = "X"
-                }
-            },
-            isSunk() {
-                return !!this.position.length && this.position.every(segment => segment === "X")
-            },
-        },
-        {
-            type: "Battleship",
-            length: 4,
-            position: [],
-            orientation: [
-                [0, 1, 2, 3],
-                [0, 10, 20, 30]
-            ],
-            hit(segment) {
-                if(segment < this.position.length) {
-                    this.position[segment] = "X"
-                }
-            },
-            isSunk() {
-                return !!this.position.length && this.position.every(segment => segment === "X")
-            },
-        },
-        {
-            type: "Carrier",
-            length: 5,
-            position: [],
-            orientation: [
-                [0, 1, 2, 3, 4],
-                [0, 10, 20, 30, 40]
-            ],
-            hit(segment) {
-                if(segment < this.position.length) {
-                    this.position[segment] = "X"
-                }
-            },
-            isSunk() {
-                return !!this.position.length && this.position.every(segment => segment === "X")
-            },
-        },
-    ]
+
+    // Takes segment and marks it as hit.
+    function hit(segment) {
+        this.position[segment] = "X"
+    }
+
+    // Checks all segments in position to see if ship is fully sunk.
+    function isSunk() {
+        return !!this.position.length && this.position.every(segment => segment === "X")
+    }
 
     // Checks if all ships in the fleet are sunk.
     const checkIfShipsSunk = () => {
         return ships.every(ship => ship.isSunk())
     }
+
+    const ships = [
+        {
+            type: "Destroyer",
+            position: [],
+            orientation: [
+                [0, 1],
+                [0, 10]
+            ],
+            hit: hit,
+            isSunk: isSunk,
+        },
+        {
+            type: "Submarine",
+            position: [],
+            orientation: [
+                [0, 1, 2],
+                [0, 10, 20]
+            ],
+            hit: hit,
+            isSunk: isSunk,
+        },
+        {
+            type: "Cruiser",
+            position: [],
+            orientation: [
+                [0, 1, 2],
+                [0, 10, 20]
+            ],
+            hit: hit,
+            isSunk: isSunk,
+        },
+        {
+            type: "Battleship",
+            position: [],
+            orientation: [
+                [0, 1, 2, 3],
+                [0, 10, 20, 30]
+            ],
+            hit: hit,
+            isSunk: isSunk,
+        },
+        {
+            type: "Carrier",
+            position: [],
+            orientation: [
+                [0, 1, 2, 3, 4],
+                [0, 10, 20, 30, 40]
+            ],
+            hit: hit,
+            isSunk: isSunk,
+        },
+    ]
 
     return {
         ships,
